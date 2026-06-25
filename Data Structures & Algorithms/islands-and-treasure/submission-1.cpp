864dcef8633997f396bc1b678f9da77e910c1073
manan8635 <95634518+manan8635@users.noreply.github.com>
2026-06-25 13:55:44 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+
+using state = pair<int,int> ;
+#define INF 2147483647 
+int dx[4] = {-1,0,1,0} ;
+int dy[4] = {0,1,0,-1} ;
+
+bool boundary_condition(int i , int j , vector<vector<int>>& grid){
+    if(i<0|| j<0||i>=grid.size()||j>=grid[0].size()) return 0 ;
+    else return 1 ;
+}
+
+
+vector <state> valid_neighbours(int i , int j , vector<vector<int>>& grid){
+    vector <state> result ;
+
+    for(int val = 0 ; val < 4 ; val++) {
+        int nx = i + dx[val] ;
+        int ny = j + dy[val] ;
+
+// here condition will be for INF , not -1 ,. because neigbour tho 0 bhi ho skta h na nd we don't want that neighbour
+        if(boundary_condition(nx,ny,grid) && grid[nx][ny] == INF ){
+            result.push_back(make_pair(nx,ny)) ;
+        }
+
+
+        }
+
+
+    return result ;
+}
+
+    void bfs(int m , int n , queue <state > q ,vector<vector<int>>& visited, vector<vector<int>>& grid ){
+
+        while(!q.empty()){
+            state curr = q.front() ; q.pop() ;
+
+            if(visited[curr.first][curr.second]) continue ;
+            visited[curr.first][curr.second] = 1 ;
+
+            for(state x : valid_neighbours(curr.first,curr.second ,grid)){
+                if(!visited[x.first][x.second]){
+                    q.push({x.first , x.second}) ;
+                    grid[x.first][x.second] = grid[curr.first][curr.second] + 1 ;
+                }
+            }
+        }
+    }
+
+// is que me good point is distance hame grid[i][j] se hi maintain ho pa rha h 
+// so hame koi distance array ya tuple maintain krne ki jrrut nhi h
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int m = grid.size() ;
+        int n = grid[0].size() ;
+        vector<vector<int>> visited(m,vector<int>(n,0)) ;
+        queue <state > q ;
+
+        for(int i = 0 ; i < m ; i++){
+            for ( int j = 0; j < n ; j++){
+                if(grid[i][j] == 0) q.push({i,j}) ;
+            }
+        }
+
+
+        bfs(m,n,q,visited , grid) ;
+    }
+};
